a35844c73a040963b0d9e1c98bb2d466cb8d1006
Aleksander <aleksandergorecki80@gmail.com>
2020-06-12 22:09:56 +0100

    partialy style the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import{ NavLink } from 'react-router-dom';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/core/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
+
 const Header = () => (
     <header>
-        <h1>Portfolio</h1>
-        <NavLink to="/" activeClassName="is-avtive" exact={true}>Home</NavLink>
-        <NavLink to="/portfolio" activeClassName="is-avtive">Portfolio</NavLink>
-        <NavLink to="/contact" activeClassName="is-avtive">Contact</NavLink>
-    </header>
+        <AppBar
+            position="fixed"
+            className="header__app-bar"
+        >
+        <div><h1>Logo here</h1></div>
+        <div className="header__nav-group">
+            <NavLink to="/" activeClassName="is-avtive" className="header__nav-link" exact={true}>Home</NavLink>
+            <NavLink to="/portfolio" activeClassName="is-avtive" className="header__nav-link">Portfolio</NavLink>
+            <NavLink to="/contact" activeClassName="is-avtive" className="header__nav-link">Contact</NavLink>
+        </div> 
+        </AppBar>
+   </header>
 );
 
 export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const path = require('path');
 // console.log(path.join(__dirname, 'public'));
 
 module.exports = {
-    // entry: './src/app.js',
-    entry: './src/_playground/redux-101.js',
+    entry: './src/app.js',
+    // entry: './_playground/redux-101.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
