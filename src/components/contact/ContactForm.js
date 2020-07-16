86a50e5bfad0b67f2f39a365ef38e8266ab8f986
Aleksander <aleksandergorecki80@gmail.com>
2020-07-16 23:00:06 +0100

    Restructure form validation

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -4,54 +4,70 @@ import React from 'react';
 class ContactForm extends React.Component {
 
     state = {
-        nameFieldLengthValid: true,
-        nameFieldCharactersValid: true,
-        fieldIsNotEmpty: []
+            errors: [],
+            lengthDoesntMatch: [],
+            fieldIsEmpty: [],
+            wrongCharacters: [],
+        // nameFieldLengthValid: true,
+        // nameFieldCharactersValid: true,
+        
     }
 
     handleValidateName = (e) => {
-        if (this.checkIfLengthMatches(e.target.value.length, 3)) {
-            this.setState({
-                nameFieldLengthValid: false
-            })
-        } else {
-            this.setState({
-                nameFieldLengthValid: true
-            })
-        }
-        if (!this.checkIfLettersAndDigits(e.target.value)) {
-            this.setState({
-                nameFieldCharactersValid: false
-            })
-        } else {
-            this.setState({
-                nameFieldCharactersValid: true
-            })
-        }
-        if (this.checkIfEmpty(e.target.value)) {
-            // dodawanie pola do tabeli błędów
-            const name = e.target.name;
-            if(this.findInArray(name)) return;
-            this.setState((prevState)=>{
-                return {
-                    fieldIsNotEmpty: [...prevState.fieldIsNotEmpty, name]
-                }
-            })
+        const name = e.target.name;
 
-        } else {
-            // usuwanie pola z tabeli błędów
-            if(!this.findInArray(name)) return;
-            const newArr = this.state.fieldIsNotEmpty.filter((element)=>{
-                return element !== name;
-            })
-            this.setState({
-                    fieldIsNotEmpty: newArr
-            })    
+        const isEmpty = this.checkIfEmpty(e.target.value);
+        
+        
+        if (this.checkIfEmpty(e.target.value)) {
+            const errorToAdd = {message: 'Is empty', name: name}
+            this.addError(errorToAdd);
         }
+
+        if (!this.checkIfLengthMatches(e.target.value, 3)){
+            const errorToAdd = {message: 'Too short', name: name};
+            this.addError(errorToAdd);
+        }
+
+        const lengthMatches = this.checkIfLengthMatches(e.target.value, 3);
+        console.log('lengthMatches', lengthMatches, name);
+
+        const lettersAndDigits = this.checkIfLettersAndDigits(e.target.value);
+        // console.log('lettersAndDigits', lettersAndDigits, name);
+
+
+
+        // if (this.checkIfEmpty(e.target.value)) {
+        //     // dodawanie pola do tabeli błędów            
+        //     if(this.findInArray(name)) return;
+        //     this.setState((prevState)=>{
+        //         return {
+        //                 fieldIsEmpty: [...prevState.fieldIsEmpty, name]
+        //         }
+        //     })
+        // } else {
+        //     // usuwanie pola z tabeli błędów
+        //     if(!this.findInArray(name)) return;
+        //     const newArr = this.state.fieldIsEmpty.filter((element)=>{
+        //         return element !== name;
+        //     })
+        //     this.setState({
+        //                 fieldIsEmpty: newArr
+        //         }
+        //     )  
+        // }
+    }
+
+    addError(errorToAdd){
+        this.setState((prevState)=>{
+            return {
+                errors: [ ...prevState.errors, {message: errorToAdd.message , name: errorToAdd.name} ]
+            }
+        })
     }
 
     findInArray = (phrase) => {
-        return this.state.fieldIsNotEmpty.find((phraseInArray)=>{
+        return this.state.fieldIsEmpty.find((phraseInArray)=>{
             return phraseInArray === phrase;
         })
     }
@@ -73,11 +89,9 @@ class ContactForm extends React.Component {
             <div className="contact-form">
                 <form id="contact" action="" method="post">
                     <input type="text" name="name" placeholder="Name" id="name" onBlur={this.handleValidateName}></input>
-                    {!this.state.nameFieldLengthValid && <span>Name must be at least 3 characters long</span>}
-                    {!this.state.nameFieldCharactersValid && <span>Name field must contain only letters and numbers.</span>}
-                    {this.state.fieldIsNotEmpty['name'] && <span>Name field must not be empty.</span>}
+                    {/* {this.state.errors.fieldIsNotEmpty['name'] && <span>Name field must not be empty.</span>} */}
                     <input type="email" name="email" placeholder="Email" id="email" onBlur={this.handleValidateName}></input>
-                    {this.state.fieldIsNotEmpty.find((element)=> element === 'email') && <span>Email field must not be empty.</span>}
+                    {/* {this.state.errors.fieldIsNotEmpty.find((element)=> element === 'email') && <span>Email field must not be empty.</span>} */}
                     <input type="text" name="subject" placeholder="Subject" id="subject" ></input>
                     <span></span>
                     <textarea placeholder="Message" name='message' id="message"></textarea>
