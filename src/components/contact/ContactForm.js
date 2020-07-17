adb633012de178123e4f4841dabb99731932ab79
Aleksander <aleksandergorecki80@gmail.com>
2020-07-17 23:58:38 +0100

    Form validation adds and removes errors using state. Not display yet

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,86 +1,84 @@
 import React from 'react';
-
+const _ = require('lodash');
 
 class ContactForm extends React.Component {
 
     state = {
-            errors: [],
-
-        // nameFieldLengthValid: true,
-        // nameFieldCharactersValid: true,
-        
+        errors: [],
+        messages: {
+            isEmpty: 'This field can not be empty.',
+            tooShort: 'This field must contain three or mor caracters.',
+            lettersAndNumbers: 'Only digit and numbers are allowed',
+            containCharacters: 'Inproper email adress.'
+        },
+        minimumLength: 3
     }
 
-    handleValidateName = (e) => {
+    handleValidateNameAndSubject = (e) => {
         const name = e.target.name;
-
-        const isEmpty = this.checkIfEmpty(e.target.value);
-        
-        
         if (this.checkIfEmpty(e.target.value)) {
-            if(this.findInArray('Is empty', name)) return; // Do nothing
+            if (this.findInArray(this.state.messages.isEmpty, name)) return; // Do nothing
             // Add error to the array
-            const errorToAdd = {message: 'Is empty', name: name}
+            const errorToAdd = { message: this.state.messages.isEmpty, name: name }
             this.addError(errorToAdd);
         } else {
-            console.log('tutaj')
-            console.log(name)
-                
-            // Remove an error
-            const newArr = this.state.errors.filter((element)=>{
-                return element.message !== 'Is empty' && element.name !== name;
-            })
-            this.setState({
-                     errors: newArr
-                }
-            )
+            const errorToRemove = { message: this.state.messages.isEmpty, name: name };
+            this.removeError(errorToRemove);
+        }
+        if (this.checkIfLengthMatches(e.target.value, this.state.minimumLength)) {
+            if (this.findInArray(this.state.messages.tooShort, name)) return; // Do nothing
+            const errorToAdd = { message: this.state.messages.tooShort, name: name }
+            this.addError(errorToAdd);
+        } else {
+            const errorToRemove = { message: this.state.messages.tooShort, name: name };
+            this.removeError(errorToRemove);
         }
 
-        // if (this.checkIfLengthMatches(e.target.value, 3)){
-        //     const errorToAdd = {message: 'Too short', name: name};
-        //     this.addError(errorToAdd);
-        // }
-
-        // if(this.checkIfLettersAndDigits(e.target.value)) {
-        //     const errorToAdd = {message: 'Only letters and digits are allowed.', name: name};
-        //     this.addError(errorToAdd)
-        // }
-
-
-
-
-
-        // if (this.checkIfEmpty(e.target.value)) {
-        //     // dodawanie pola do tabeli błędów            
-        //     if(this.findInArray(name)) return;
-        //     this.setState((prevState)=>{
-        //         return {
-        //                 fieldIsEmpty: [...prevState.fieldIsEmpty, name]
-        //         }
-        //     })
-        // } else {
-        //     // usuwanie pola z tabeli błędów
-        //     if(!this.findInArray(name)) return;
-        //     const newArr = this.state.fieldIsEmpty.filter((element)=>{
-        //         return element !== name;
-        //     })
-        //     this.setState({
-        //                 fieldIsEmpty: newArr
-        //         }
-        //     )  
-        // }
+        if (!this.checkIfLettersAndDigits(e.target.value)) {
+            if (this.findInArray(this.state.messages.lettersAndNumbers, name)) return; // Do nothing
+            const errorToAdd = { message: this.state.messages.lettersAndNumbers, name: name }
+            this.addError(errorToAdd);
+        } else {
+            const errorToRemove = { message: this.state.messages.lettersAndNumbers, name: name };
+            this.removeError(errorToRemove);
+        }
+    }
+    handleValidateEmail = (e) => {
+        const name = e.target.name;
+        if (!this.containCharacters(e.target.value)) {
+            if (this.findInArray(this.state.messages.containCharacters, name)) return; // Do nothing
+            // Add error to the array
+            const errorToAdd = { message: this.state.messages.containCharacters, name: name }
+            this.addError(errorToAdd);
+        } else {
+            const errorToRemove = { message: this.state.messages.containCharacters, name: name };
+            this.removeError(errorToRemove);
+        }
     }
 
-    addError(errorToAdd){
-        this.setState((prevState)=>{
+    addError(errorToAdd) {
+        console.log(errorToAdd)
+        this.setState((prevState) => {
             return {
-                errors: [ ...prevState.errors, {message: errorToAdd.message , name: errorToAdd.name} ]
+                errors: [...prevState.errors, { message: errorToAdd.message, name: errorToAdd.name }]
             }
         })
     }
 
+    removeError = (errorToRemove) => {
+        this.setState((prevState) => {
+            const newArr = prevState.errors.filter((element) => {
+                return !_.isEqual(element, errorToRemove);
+            });
+            return {
+                errors: newArr
+            }
+        })
+
+    }
+
     findInArray = (message, name) => {
-        return this.state.errors.find((error)=>{
+        return this.state.errors.find((error) => {
             return error.message === message && error.name === name;
         })
     }
@@ -89,26 +87,38 @@ class ContactForm extends React.Component {
         return (phrase.trim()) === '' ? true : false;
     }
 
-    checkIfLengthMatches = (phrase, length) =>  phrase.length < length;
-    
+    // checkIfLengthMatches = (phrase, phraseLength) =>  phrase.length < phraseLength;
+    checkIfLengthMatches = (phrase, phraseLength) => {
+        return phrase.length < phraseLength;
+    }
+
     checkIfLettersAndDigits = (phrase) => {
         const regEx = /^[a-zA-Z0-9 ]+$/;
-        if(regEx.test(phrase)){
+        if (regEx.test(phrase)) {
             return true
         }
     }
+
+    containCharacters = (phrase) => {
+        const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (regEx.test(phrase)) {
+            return true
+        }
+    }
+
     render() {
         return (
             <div className="contact-form">
                 <form id="contact" action="" method="post">
-                    <input type="text" name="name" placeholder="Name" id="name" onBlur={this.handleValidateName}></input>
+                    <input type="text" name="name" placeholder="Name" id="name" onBlur={this.handleValidateNameAndSubject}></input>
                     {/* {this.state.errors.fieldIsNotEmpty['name'] && <span>Name field must not be empty.</span>} */}
-                    <input type="email" name="email" placeholder="Email" id="email" onBlur={this.handleValidateName}></input>
+                    <input type="email" name="email" placeholder="Email" id="email" onBlur={this.handleValidateEmail}></input>
                     {/* {this.state.errors.fieldIsNotEmpty.find((element)=> element === 'email') && <span>Email field must not be empty.</span>} */}
-                    <input type="text" name="subject" placeholder="Subject" id="subject" ></input>
+                    <input type="text" name="subject" placeholder="Subject" id="subject" onBlur={this.handleValidateNameAndSubject}></input>
                     <span></span>
                     <textarea placeholder="Message" name='message' id="message"></textarea>
                     <button id="submitButton" name="submit" className="button button--green button__contact-form" >Send</button>
+
                 </form>
             </div>
         )
